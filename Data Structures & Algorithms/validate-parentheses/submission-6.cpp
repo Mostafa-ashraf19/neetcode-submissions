72b5cfdba0b364652d49e654ca2b37b31aada5f4
Mostafa Ashraf <mostaphaashraf1996@gmail.com>
2026-06-13 11:31:33 +0300

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stc;
+
+        for(char c : s) {
+            
+            if(c == '(' || c == '{' || c == '[' ) {
+                stc.push(c);
+            } else {
+                if(stc.empty())
+                    return false;
+                
+                char tp = stc.top();
+                stc.pop();
+                if((tp == '(' && c != ')') ||
+                (tp == '{' && c != '}')  ||
+                (tp == '[' && c != ']'))
+                    return false;
+            }
+        }
+        return stc.empty() == true ;
+    }
+};
